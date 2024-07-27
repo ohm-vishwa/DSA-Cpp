01b7c03d8dbc72974433d25776d064772a082dfe
ohm-vishwa <ohmdto1513@gmail.com>
2024-07-27 14:18:21 +0530

    updation

---
+++ Problems/0_main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include "1_fib.cpp"
+#include "2_isPrime.cpp"
+#include "3_subtractProductAndSum.cpp"
+#include "4_decimalToBinary.cpp"
+
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the value of n : ";
+    cin >> n;
+
+    cout << "\nfib of " << n << " is " << fib(n) << " " << endl;
+
+    isPrime(n);
+
+    cout << "product - sum = " << subtractProductAndSum(n) << endl;
+
+    cout << "Binary of " << n <<" is "; 
+    decimalToBinary(n);
+    cout << endl;
+    return 0;
+}
