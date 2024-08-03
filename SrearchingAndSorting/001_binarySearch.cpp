cdbf26fc2e297842c9488488a6cf80e4689ef821
ohm-vishwa <ohmdto1513@gmail.com>
2024-08-03 19:38:07 +0530

    updation

--- SrearchingAndSorting/001_binarySearch.cpp
+++ SrearchingAndSorting/001_binarySearch.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int binarySearch(vector<int> arr, int key){
+    int start = 0;
+    int end = arr.size() -1;
+    int mid = (start >> 1) + (end >> 1);
+
+    while(end != start){
+        cout << mid << endl;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] > key){
+            end = mid -1;
+        }
+        else {
+            start = mid +1;
+        }
+        mid = (start >> 1) + (end >> 1);
+    }
+    return -1;
+}
+
 int main(){
-    
+    vector <int> arr = {1,2,3,5,6,7,8,9,23,45,67,89};
+    int index = binarySearch(arr,3);
+    // int index = 0;
+    cout << "key is : " << index << endl;
     return 0;
 }
