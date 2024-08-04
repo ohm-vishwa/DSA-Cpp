52a95d0d2a400287da1bb17a106fd661f729af2e
ohm-vishwa <ohmdto1513@gmail.com>
2024-08-04 23:42:01 +0530

    updation

--- SrearchingAndSorting/005_findPeakElement.cpp
+++ SrearchingAndSorting/005_findPeakElement.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 
 int findPeakElement(vector<int> arr){
-    int peak = INT_MIN;
+    int peak = -1;
     int start = 0;
     int end = arr.size()-1;
     int mid;
 
     while(end >= start){
         mid = start + (end - start) / 2;
+        peak = arr[mid];
 
         if((arr[mid] > arr[mid+1]) && (arr[mid] > arr[mid-1])){
-            return arr[mid];
+            return peak;
         }
         if(arr[mid] < arr[mid+1]){
             start = mid+1;
@@ -21,16 +22,13 @@ int findPeakElement(vector<int> arr){
         else{
             end = mid;
         }
-        
-        if(end == mid){
-            return arr[mid];
-        }
     }
-    return -1;
+        cout << mid;
+    return peak;
 }
 
 int main(){
-    vector <int> arr = {1,2,3,5,6,7,8,9,10}; // peak element is 7
+    vector <int> arr = {1,2,3,5,6,3,2,1}; // peak element is 7
     int peak = findPeakElement(arr);
     cout << "peak element is : " << peak << endl;
     return 0;
